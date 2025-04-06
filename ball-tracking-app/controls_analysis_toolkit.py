5544707e7a52fa067e690eb2c9d47aded8cce5df
Moe <muhtasim1023@gmail.com>
2025-04-06 14:16:29 -0400

    create a controls toolkit to plot essentail data

---
+++ ball-tracking-app/controls_analysis_toolkit.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+from PyQt5 import QtGui, QtCore
+import pyqtgraph as pg
+import typing
+
+class Graph:
+
+    def __init__(self):
+        pass
+
+
+if __name__ == "__main__":
