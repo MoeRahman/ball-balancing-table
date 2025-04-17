4a564e9b605db04577008a8d98d3dfb07c08bb16
Moe <muhtasim1023@gmail.com>
2025-04-16 21:20:46 -0400

    created plotting tool for analysing data in real time

---
+++ ball-tracking-app/controls_analysis_tool.py
@@ -0,0 +1,111 @@
+import sys
+import pyqtgraph as pg
+from PyQt5.QtWidgets import QApplication, QMainWindow, QHBoxLayout, QGridLayout, QWidget
+from PyQt5.QtCore import QTimer
+
+class ControlsWindow(QMainWindow):
+
+    def __init__(self):
+        super().__init__()
+        self.setWindowTitle("Real-Time Controls Plot")
+        self.setGeometry(100, 100, 800, 600)
+
+        central_widget = QWidget(self)
+        self.setCentralWidget(central_widget)
+        main_layout = QHBoxLayout()
+        central_widget.setLayout(main_layout)
+
+        grid_widget = QWidget()
+        grid_layout = QGridLayout()
+        grid_widget.setLayout(grid_layout)
+        main_layout.addWidget(grid_widget)
+
+        self.plot_widgets = []
+        self.curves = []
+        self.pairs = [
+            ['x-setpoint', 'x-measurement'],
+            ['y-setpoint', 'y-measurement']
+        ]
+        titles = [
+            'X-Axis', 'Y-Axis'
+        ]
+
+        for i, (col1, col2) in enumerate(self.pairs):
+            plot = pg.PlotWidget(background='w')
+            plot.showGrid(x=True, y=True)
+            plot.setTitle(titles[i])
+            plot.setLabel('left', 'Value')
+            plot.setLabel('bottom', 'Time Step')
+
+            curve1 = plot.plot(pen=pg.mkPen('r', width=2), name=col1)
+            curve2 = plot.plot(pen=pg.mkPen('b', width=2), name=col2)
+
+            self.plot_widgets.append(plot)
+            self.curves.append((curve1, curve2))
+
+            row, col = divmod(i, 2)
+            grid_layout.addWidget(plot, row, col)
+
+        self.t = []
+        self.data1 = []
+        self.data2 = []
+        self.data3 = []
+        self.data4 = []
+
+    def update_data(self, t, plot1, plot2):
+        max_len = 100
+
+        if len(self.t) >= max_len:
+            self.t.pop(0)
+            self.data1.pop(0)
+            self.data2.pop(0)
+            self.data3.pop(0)
+            self.data4.pop(0)
+
+        self.t.append(t)
+        self.data1.append(plot1[0])
+        self.data2.append(plot1[1])
+        self.data3.append(plot2[0])
+        self.data4.append(plot2[1])
+
+        for i, _ in enumerate(self.pairs):
+            curve1, curve2 = self.curves[i]
+            if i == 0:
+                y1 = self.data1
+                y2 = self.data2
+            else:
+                y1 = self.data3
+                y2 = self.data4
+            x = list(range(len(y1)))
+            curve1.setData(x, y1)
+            curve2.setData(x, y2)
+
+
+def main():
+    app = QApplication(sys.argv)
+    win = ControlsWindow()
+    win.show()
+
+    # Demo data update
+    timer = QTimer()
+    t = 0
+
+    def update():
+        nonlocal t
+        import math
+        x_set = 100 * math.sin(0.1 * t)
+        x_meas = x_set + 10 * math.sin(0.3 * t)
+        y_set = 100 * math.cos(0.1 * t)
+        y_meas = y_set + 5 * math.sin(0.2 * t)
+
+        win.update_data(t, [x_set, x_meas], [y_set, y_meas])
+        t += 1
+
+    timer.timeout.connect(update)
+    timer.start(100)
+
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    main()
--- ball-tracking-app/main.py
+++ ball-tracking-app/main.py
@@ -1,60 +1,58 @@
 
 import math
 import cv2 as cv
+from cv2 import VideoCapture
 import numpy as np
+from PyQt5 import QtWidgets
 import serial
-import joystick 
+import sys
+import threading
+import time
+from joystick import XboxController 
 import ball_tracking as ball
+from controls_analysis_tool import ControlsWindow
 import inverse_kinematics as ik
 
+height = 50
+roll_err = 0
+pitch_err = 0
 
+x_vel_error = 0
+y_vel_error = 0
 
-def main() -> None:
+alpha = 1
+prev_x_vel_error = 0
+prev_y_vel_error = 0
 
-    controller = joystick.XboxController()
+cumulative_roll_err = 0
+cumulative_pitch_err = 0
 
-    video_capture = cv.VideoCapture(0)
+kp_roll  = 0.01
+kp_pitch = 0.01
+kd_roll  = 0.1
+kd_pitch = 0.1
+ki_roll  = 0
+ki_pitch = 0
+servo_offsets = [0, 0, 0]
 
-    arduino_serial = serial.Serial(port='COM13', baudrate=115200, timeout=0.1)
+filtered_coordinates = ball.kalmanInit()
+prev_point = np.array([0,0], dtype=np.float32)
 
-    if arduino_serial.is_open:
-        arduino_serial.close()
+def ball_balance(plot_window: ControlsWindow, controller: XboxController, video_capture: VideoCapture, arduino_serial: serial.Serial) -> None:
 
-    arduino_serial.open()
-
-    height = 50
-    roll_err = 0
-    pitch_err = 0
-
-    x_vel_error = 0
-    y_vel_error = 0
-
-    alpha = 0.8
-    prev_x_vel_error = 0
-    prev_y_vel_error = 0
-
-    cumulative_roll_err = 0
-    cumulative_pitch_err = 0
-
-    kp_roll  = 0.01
-    kp_pitch = 0.01
-    kd_roll  = 0.1
-    kd_pitch = 0.1
-    ki_roll  = 0
-    ki_pitch = 0
-    servo_offsets = [0, 0, 0]
-
-    filtered_coordinates = ball.kalmanInit()
-    prev_point = np.array([0,0], dtype=np.float32)
-
-    position_setpoint = [0,0]
-    velocity_setpoint = [0, 0]
+    global height, roll_err, pitch_err
+    global x_vel_error, y_vel_error
+    global alpha, prev_x_vel_error, prev_y_vel_error
+    global cumulative_roll_err, cumulative_pitch_err
+    global kp_roll, kp_pitch, kd_roll, kd_pitch, ki_roll, ki_pitch
+    global servo_offsets
+    global filtered_coordinates
+    global prev_point
 
     while True:
+        t = time.perf_counter()
 
-        position_setpoint[0] = 200 * controller.read()[0]
-        position_setpoint[1] = 200 * controller.read()[1]
-
+        position_setpoint = [200 * controller.read()[0], 200 * controller.read()[1]]
         reset_position = controller.read()[2]
 
         ret, ball_coordinate = ball.track(video_capture)
@@ -89,116 +87,112 @@ def main() -> None:
 
         if len(ball_coordinate) == 0:
             pass
-        else:
 
-            #obejct_coordinates = f'{ball_coordinate[0]}, {ball_coordinate[1]}\n'
-            coordinate_display = f'{ball_coordinate[0]}, {ball_coordinate[1]}'
+        #obejct_coordinates = f'{ball_coordinate[0]}, {ball_coordinate[1]}\n'
+        coordinate_display = f'{ball_coordinate[0]}, {ball_coordinate[1]}'
 
-            # Calculate Velocity and Position Error
-            roll_err  = predicted_position[1] - position_setpoint[1]
-            pitch_err = predicted_position[0] - position_setpoint[0]
+        # Calculate Velocity and Position Error
+        roll_err  = predicted_position[1] - position_setpoint[1]
+        pitch_err = predicted_position[0] - position_setpoint[0]
 
-            x_vel_error = predicted_position[3] - velocity_setpoint[1]
-            y_vel_error = predicted_position[2] - velocity_setpoint[0]
+        x_vel_error = predicted_position[3]
+        y_vel_error = predicted_position[2]
 
-            # Position Error Deadband
-            position_deadband = 35
-            if(roll_err < position_deadband and roll_err > -position_deadband):
-                roll_err = 0
-                cumulative_roll_err = cumulative_roll_err
+        # Position Error Deadband
+        position_deadband = 35
+        if(roll_err < position_deadband and roll_err > -position_deadband):
+            roll_err = 0
+            cumulative_roll_err = cumulative_roll_err
 
-            if(pitch_err < position_deadband and pitch_err > -position_deadband):
-                pitch_err = 0
-                cumulative_pitch_err = cumulative_pitch_err
+        if(pitch_err < position_deadband and pitch_err > -position_deadband):
+            pitch_err = 0
+            cumulative_pitch_err = cumulative_pitch_err
 
-            # Resolving Derivative/Velocity Kick by filtering velocity signal
-            filtered_x_vel_error = alpha * x_vel_error + (1 - alpha) * prev_x_vel_error 
-            filtered_y_vel_error = alpha * y_vel_error + (1 - alpha) * prev_y_vel_error
+        # Resolving Derivative/Velocity Kick by filtering velocity signal
+        filtered_x_vel_error = alpha * x_vel_error + (1 - alpha) * prev_x_vel_error 
+        filtered_y_vel_error = alpha * y_vel_error + (1 - alpha) * prev_y_vel_error
 
-            prev_x_vel_error = filtered_x_vel_error
-            prev_y_vel_error = filtered_y_vel_error
+        prev_x_vel_error = filtered_x_vel_error
+        prev_y_vel_error = filtered_y_vel_error
 
-            # Velocity Error Deadband
-            velocity_deadband = 2
-            if(filtered_x_vel_error < velocity_deadband and filtered_x_vel_error > -velocity_deadband):
-                filtered_x_vel_error = 0
+        # Velocity Error Deadband
+        velocity_deadband = 2
+        if(filtered_x_vel_error < velocity_deadband and filtered_x_vel_error > -velocity_deadband):
+            filtered_x_vel_error = 0
 
-            if(filtered_y_vel_error < velocity_deadband and filtered_y_vel_error > -velocity_deadband):
-                filtered_y_vel_error = 0
+        if(filtered_y_vel_error < velocity_deadband and filtered_y_vel_error > -velocity_deadband):
+            filtered_y_vel_error = 0
 
-            # Velocity Error Clamp
-            velocity_clamp = 60
-            if(filtered_x_vel_error > velocity_clamp):
-                filtered_x_vel_error = velocity_clamp
+        # Velocity Error Clamp
+        velocity_clamp = 60
+        if(filtered_x_vel_error > velocity_clamp):
+            filtered_x_vel_error = velocity_clamp
 
-            if(filtered_x_vel_error < -1*velocity_clamp):
-                filtered_x_vel_error = -1*velocity_clamp
+        if(filtered_x_vel_error < -1*velocity_clamp):
+            filtered_x_vel_error = -1*velocity_clamp
 
-            if(filtered_y_vel_error > velocity_clamp):
-                filtered_y_vel_error = velocity_clamp
+        if(filtered_y_vel_error > velocity_clamp):
+            filtered_y_vel_error = velocity_clamp
 
-            if(filtered_y_vel_error < -1*velocity_clamp):
-                filtered_y_vel_error = -1*velocity_clamp
+        if(filtered_y_vel_error < -1*velocity_clamp):
+            filtered_y_vel_error = -1*velocity_clamp
 
-            # Cumulative Erorr
-            cumulative_roll_err  += roll_err
-            cumulative_pitch_err += pitch_err
+        # Cumulative Erorr
+        cumulative_roll_err  += roll_err
+        cumulative_pitch_err += pitch_err
 
-            # Clamp Cumulative Eror {Mitigate Integral Windup}
-            integral_clamp = 20000
-            if(abs(cumulative_roll_err) > integral_clamp):
-                if(cumulative_roll_err < 0):
-                    cumulative_roll_err = -1*integral_clamp
-                else:
-                    cumulative_roll_err = integral_clamp
+        # Clamp Cumulative Eror {Mitigate Integral Windup}
+        integral_clamp = 20000
+        if(abs(cumulative_roll_err) > integral_clamp):
+            if(cumulative_roll_err < 0):
+                cumulative_roll_err = -1*integral_clamp
+            else:
+                cumulative_roll_err = integral_clamp
 
-            if(abs(cumulative_pitch_err) > integral_clamp):
-                if(cumulative_pitch_err == 0):
-                    cumulative_pitch_err = -1*integral_clamp
-                else:
-                    cumulative_pitch_err = integral_clamp
+        if(abs(cumulative_pitch_err) > integral_clamp):
+            if(cumulative_pitch_err == 0):
+                cumulative_pitch_err = -1*integral_clamp
+            else:
+                cumulative_pitch_err = integral_clamp
 
-            # Calculate the PID Output 
-            roll_out  = kp_roll*roll_err   + kd_roll *filtered_x_vel_error  + ki_roll*cumulative_roll_err
-            pitch_out = kp_pitch*pitch_err + kd_pitch*filtered_y_vel_error + ki_pitch*cumulative_pitch_err 
+        # Calculate the PID Output 
+        roll_out  = kp_roll*roll_err   + kd_roll *filtered_x_vel_error + ki_roll*cumulative_roll_err
+        pitch_out = kp_pitch*pitch_err + kd_pitch*filtered_y_vel_error + ki_pitch*cumulative_pitch_err 
 
-            # Roll and Pitch Output Clamp
-            clamp_val = 8
-            if(abs(roll_out) > clamp_val):
-                roll_out = clamp_val
+        # Roll and Pitch Output Clamp
+        clamp_val = 10
+        if(abs(roll_out) > clamp_val):
+            roll_out = clamp_val
 
-            if(abs(pitch_out) > clamp_val):
-                pitch_out = clamp_val
-           
-            servo_angles = ik.calculate_joint_angles(roll=roll_out, pitch=pitch_out, height=height)
-            servo_angles = np.around(servo_angles, decimals=2) + servo_offsets
+        if(abs(pitch_out) > clamp_val):
+            pitch_out = clamp_val
+        
+        servo_angles = ik.calculate_joint_angles(roll=roll_out, pitch=pitch_out, height=height)
+        servo_angles = np.around(servo_angles, decimals=2) + servo_offsets
 
-            # print(float("{:.2f}".format(roll_out)), float("{:.2f}".format(pitch_out)), 
-            #       float("{:.2f}".format(roll_err)), float("{:.2f}".format(pitch_err)),
-            #       float("{:.2f}".format(filtered_x_vel_error)), float("{:.2f}".format(filtered_y_vel_error)),
-            #       float("{:.2f}".format(cumulative_roll_err)), float("{:.2f}".format(cumulative_pitch_err)),
-            #       float("{:.2f}".format(servo_angles[0])), float("{:.2f}".format(servo_angles[1])), float("{:.2f}".format(servo_angles[2])))
+        # To avoid the issue with Nan values we can just round those numbers to 90 deg
+        servo_angles = [90.0 if math.isnan(val) else val for val in servo_angles]
             
-            # To avoid the issue with Nan values we can just round those numbers to 90 deg
-            servo_angles = [90.0 if math.isnan(val) else val for val in servo_angles]
-                
-            # Serial Communication
-            serial_message = ",".join([str(float(val)) for val in servo_angles])
-            #print(serial_message)
-            arduino_serial.write(serial_message.encode('utf-8'))
-            arduino_serial.write(b'\n')
+        # Serial Communication
+        serial_message = ",".join([str(float(val)) for val in servo_angles])
+        arduino_serial.write(serial_message.encode('utf-8'))
+        arduino_serial.write(b'\n')
 
-            coordinate_display = f'{coordinate_measurement[0]}, {coordinate_measurement[1]}'
+        coordinate_display = f'{coordinate_measurement[0]}, {coordinate_measurement[1]}'
 
-            cv.circle(img=frame, center=(int(position_setpoint[0]) + 500, int(position_setpoint[1]) + 500), radius=40, color=(0,0,255), lineType=cv.LINE_AA, thickness=2)
-            cv.circle(img=frame, center=(500, 500), radius=450, color=(0,0,0), lineType=cv.LINE_AA, thickness=1)
-            cv.circle(img=frame, center=(coordinate_measurement[0].astype(int) + 500, coordinate_measurement[1].astype(int) + 500), radius=2, color=(255,0,255), lineType=cv.LINE_AA, thickness=2)
-            cv.putText(img=frame, text=coordinate_display, org=(coordinate_measurement[0].astype(int)+540, coordinate_measurement[1].astype(int)+500), fontFace=cv.FONT_HERSHEY_PLAIN, fontScale=1, color=(0, 0, 0), lineType=cv.LINE_AA, thickness=1)
+        cv.circle(img=frame, center=(int(position_setpoint[0]) + 500, int(position_setpoint[1]) + 500), radius=40, color=(0,0,255), lineType=cv.LINE_AA, thickness=2)
+        cv.circle(img=frame, center=(500, 500), radius=450, color=(0,0,0), lineType=cv.LINE_AA, thickness=1)
+        cv.circle(img=frame, center=(coordinate_measurement[0].astype(int) + 500, coordinate_measurement[1].astype(int) + 500), radius=2, color=(255,0,255), lineType=cv.LINE_AA, thickness=2)
+        cv.putText(img=frame, text=coordinate_display, org=(coordinate_measurement[0].astype(int)+540, coordinate_measurement[1].astype(int)+500), fontFace=cv.FONT_HERSHEY_PLAIN, fontScale=1, color=(0, 0, 0), lineType=cv.LINE_AA, thickness=1)
 
         coordinate_display = f'{predicted_position[0].astype(int)}, {predicted_position[1].astype(int)}'
 
         cv.circle(img=frame, center=(predicted_position[0].astype(int)+500, predicted_position[1].astype(int)+500), radius=2, color=(255,0,0), lineType=cv.LINE_AA, thickness=2)
         cv.putText(img=frame, text=coordinate_display, org=(predicted_position[0].astype(int)+540, predicted_position[1].astype(int)+540), fontFace=cv.FONT_HERSHEY_PLAIN, fontScale=1, color=(0, 0, 0), lineType=cv.LINE_AA, thickness=1)
+
+        # Plot data for controls analysis
+        plot_window.update_data(t, plot1 = [position_setpoint[0], predicted_position[0]],
+                                   plot2 = [position_setpoint[1], predicted_position[1]])
 
         # Reference lines to align the platform
         for angle in range(30,275,120):
@@ -206,8 +200,10 @@ def main() -> None:
             ry = int(475*np.sin(np.deg2rad(angle)))
             cv.line(img=frame, pt1=(500,500), pt2=(500-rx,500-ry), color=(0,0,0), thickness=1)
 
+
+        frame = cv.resize(frame, (500, 500))
         # DISPLAY IMAGE
-        cv.imshow('video-raw', frame)
+        cv.imshow('Top_Down_View', frame)
 
         # Quit program
         if cv.waitKey(1) & 0xFF == ord('q'):
@@ -220,5 +216,30 @@ def main() -> None:
     cv.destroyAllWindows()
 
     return None
+
+
+def main():
+
+    app = QtWidgets.QApplication(sys.argv)
+    plot_window = ControlsWindow()
+    plot_window.show()
+
+    controller = XboxController()
+
+    video_capture = VideoCapture(0)
+
+    arduino_serial = serial.Serial(port='COM13', baudrate=115200, timeout=0.1)
+
+    if arduino_serial.is_open:
+        arduino_serial.close()
+
+    arduino_serial.open()
+
+    # Run control loop in a thread
+    control_thread = threading.Thread(target=ball_balance, args=(plot_window,controller,video_capture,arduino_serial))
+    control_thread.start()
+
+    sys.exit(app.exec_())
+
 
 main()
